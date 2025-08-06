08c980564a1c5ff512be891988379b2a178e047f
irina-brightside <mia.irina.98@gmail.com>
2025-08-06 20:25:14 +0300

    Добавлен шаг 2 — голосование

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def main():
     
 @dp.startup()
 async def schedule_vote():
-    scheduler.add_job(send_vote_poll, CronTrigger(day_of_week='wed', hour=20, minute=23, timezone="Europe/Moscow"))
+    scheduler.add_job(send_vote_poll, CronTrigger(day_of_week='wed', hour=20, minute=28, timezone="Europe/Moscow"))
 
 scheduler.start()
 
@@ -82,10 +82,10 @@ async def send_vote_poll():
         allows_multiple_answers=True
     )
 
-movie_links.clear()
+    movie_links.clear()
 
-await bot.delete_webhook(drop_pending_updates=True)
-await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
